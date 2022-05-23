c8f2c4f8c12b6e731b867227f45742c4b4dda7b2
YeongSeok Kim <youngstone89@icloud.com>
2022-05-23 13:51:44 +0900

    added helloworld grpc testing

--- 3rdParty/Nginx-Ingress/Azure/grpc-go/scripts/grpc_connection.sh
+++ 3rdParty/Nginx-Ingress/Azure/grpc-go/scripts/grpc_connection.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-$ grpcurl grpc-test.eastus.cloudapp.azure.com:443/grpc helloworld.Greeter/SayHello
+$ grpcurl grpc-test.eastus.cloudapp.azure.com:443 helloworld.Greeter/SayHello
 {
   "message": "Hello "
 }
--- 3rdParty/Nginx-Ingress/Azure/grpc-go/src/main.go
+++ 3rdParty/Nginx-Ingress/Azure/grpc-go/src/main.go
@@ -29,8 +29,9 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
+	hwpb "grpc-go/helloworld"
+
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 var port = flag.Int("port", 50051, "the port to serve on")
@@ -43,6 +44,11 @@ type hwServer struct {
 // SayHello implements helloworld.GreeterServer
 func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
 	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *hwServer) SayHelloAgain(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
+	return &hwpb.HelloReply{Message: "Hello Again " + in.Name}, nil
 }
 
 type ecServer struct {
